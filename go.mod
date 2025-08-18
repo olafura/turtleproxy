320e5cdd73459a1cc177835dd2efe0563da0ac08
Olafur Arason <olafura@olafura.com>
2025-08-18 11:01:19 +0200

    chore: modernize

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/olafura/turtleproxy
+
+go 1.24.2
+
+require (
+	github.com/dustin/go-humanize v1.0.1
+	github.com/elazarl/goproxy v1.7.2
+)
+
+require (
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
--- turtleproxy.go
+++ turtleproxy.go
@@ -5,7 +5,6 @@ import (
 	"github.com/dustin/go-humanize"
 	"github.com/elazarl/goproxy"
 	"io"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -26,21 +25,27 @@ func (c *DelayReadCloser) Read(b []byte) (n int, err error) {
 	c.bytes += int64(n)
 	return
 }
+
 func (c DelayReadCloser) Close() error {
 	endTime := time.Now()
 	timepassed := endTime.Sub(c.startTime)
 	var speed int64
+
 	if c.speedEnd == 0 {
 		speed = int64(c.speedStart)
 	} else {
 		speed = randRange(c.speedStart, c.speedEnd)
 	}
+
 	delay := time.Duration(float64(c.bytes)*8/float64(speed)*1000) * time.Millisecond
+
 	log.Println("bytes: ", c.bytes)
 	log.Println("speed: ", speed)
 	log.Println("delay: ", delay)
 	log.Println("timepassed: ", timepassed)
+
 	time.Sleep(delay - timepassed)
+
 	return c.R.Close()
 }
 
@@ -62,7 +67,6 @@ var Connections = ConnMap{
 }
 
 func randRange(min, max uint64) int64 {
-	rand.Seed(time.Now().Unix())
 	return rand.Int63n(int64(max-min)) + int64(min)
 }
 
@@ -81,9 +85,11 @@ func main() {
 	connectionArg := flag.String("c", "", conntext)
 	addrArg := flag.String("addr", ":8080", "proxy listen address")
 	flag.Parse()
+
 	if *verboseArg == false {
-		log.SetOutput(ioutil.Discard)
+		log.SetOutput(io.Discard)
 	}
+
 	if *connectionArg != "" {
 		connType, err1 := Connections[strings.ToLower(*connectionArg)]
 		if err1 == false {
@@ -100,14 +106,18 @@ func main() {
 		}
 		latencyArg = &connType.Latency
 	}
+
 	log.Println("speed", *speedHumanArg)
 	log.Println("latency", *latencyArg)
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = *verboseArg
 	speedHumanValues := strings.Split(*speedHumanArg, "-")
+
 	var speedStart uint64 = 0
 	var speedEnd uint64 = 0
 	var err2, err3, err4 error
+
 	if len(speedHumanValues) > 1 {
 		speedStart, err2 = humanize.ParseBytes(speedHumanValues[0])
 		if err2 != nil {
@@ -123,11 +133,13 @@ func main() {
 			log.Fatal(err4)
 		}
 	}
+
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		time.Sleep(time.Duration(*latencyArg) * time.Millisecond)
 		startTime := time.Now()
 		resp.Body = &DelayReadCloser{resp.Body, speedStart, speedEnd, startTime, 0}
 		return resp
 	})
+
 	log.Fatal(http.ListenAndServe(*addrArg, proxy))
 }
